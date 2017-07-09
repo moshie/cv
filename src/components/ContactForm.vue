c99f1c12d8b08143aff28b3302d2d8d02c77031c
David Hewitt <hello@wp-unite.io>
2017-07-09 12:58:03 +0100

    testing jQuery netlify form submission

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="form" name="contact" @submit.prevent="validateBeforeSubmit" v-if="!formSubmitted" :action="action" netlify>
+    <form class="form" name="contact" @submit.prevent="validateBeforeSubmit" v-if="!formSubmitted" netlify>
         <slot name="description"></slot>
 
         <div class="form__group" :class="{ 'form--has-error' : errors.has('name') }">
@@ -29,6 +29,8 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
     name: 'contact-form',
     data () {
@@ -37,38 +39,56 @@ export default {
             email: '',
             message: '',
             maxLength: 225,
-            formSubmitted: false,
-            action: 'contact-form'
+            formSubmitted: false
         }
     },
     methods: {
-        validateBeforeSubmit: function () {
+        validateBeforeSubmit: function (evt) {
             this.$validator.validateAll()
 
-            console.log(this.errors.any())
-
             if (!this.errors.any()) {
-                this.submitForm()
+                this.submitForm(evt)
             }
         },
-        submitForm: function () {
-            this.$http({
-                url: this.action,
-                method: 'POST',
-                data: JSON.stringify({
-                    name: this.name,
-                    email: this.email,
-                    message: this.message
-                })
-            }).then(() => {
-                this.formSubmitted = true
-                alert('form submitted!')
-            }, () => {
-                alert('Form submission failed!')
+        submitForm: function (evt) {
+            // var form = document.getElementsByTagName('form')[0]
+            // var data = new FormData(evt.target)
+
+            // this.$http({
+            //     url: '/',
+            //     data,
+            //     headers: {
+            //         'Content-Type': 'application/x-www-form-urlencoded'
+            //     }
+            // }).then(() => {
+            //     this.formSubmitted = true
+            //     alert('form submitted!')
+            // }, () => {
+            //     alert('Form submission failed!')
+            // })
+
+            var $form = $(evt.target)
+
+            $.post($form.attr('action'), $form.serialize()).then(function () {
+                alert('Thank you!')
             })
+
+            // this.$http({
+            //     url: '/',
+            //     method: 'POST',
+            //     data: {
+            //         name: this.name,
+            //         email: this.email,
+            //         message: this.message
+            //     }
+            // }).then(() => {
+            //     this.formSubmitted = true
+            //     alert('form submitted!')
+            // }, () => {
+            //     alert('Form submission failed!')
+            // })
         }
-    },
-    components: {}
+    }
 }
 </script>
 
